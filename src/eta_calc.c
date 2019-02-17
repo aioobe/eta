4d495fb0a4a05e920e23b8ddfbb7e153875657f8
Andreas Lundblad <andreas.lundblad@gmail.com>
2019-02-17 15:00:28 +0100

    Fixed bug with --down mode.

--- src/eta_calc.c
+++ src/eta_calc.c
@@ -22,39 +22,9 @@ double rate(Measurement *m1, Measurement *m2) {
 //
 // This function assumes that we have at least tmo measurements.
 double average_rate() {
-  
   Measurement *oldest = oldest_measurement();
   Measurement *latest = last_measurement();
-
   return rate(oldest, latest);
-
-  // The problem with the calculation below is that when using --cont we may
-  // have a situation like this:
-  //
-  //   0 <500ms> 1 <500ms> 2 <1ms> 3 <500ms> 4 <500ms> ...
-  //
-  // The average rate of change for the 2 -> 3 delta will get huge and ruin the
-  // result completely.
-
-  /*
-  // 1*rate[-k] + 2*rate[-k+1] + ... + k*rate[0]
-  // -------------------------------------------
-  //           1 + 2 + 3 + ... + k
-
-  double numerator = 0;
-  double denominator = 0;
-  int weight = 1;
-  Measurement *m = oldest_measurement();
-  do {
-    Measurement *m_prev = m;
-    m = next_measurement(m);
-    numerator += weight * rate(m_prev, m);
-    denominator += weight;
-    weight++;
-  } while (m != last_measurement());
-
-  return numerator / denominator;
-  */
 }
 
 // Calculate and return estimated seconds left.
@@ -62,23 +32,24 @@ double compute_seconds_left(value_t target_value, bool down) {
 
   Measurement *m = oldest_measurement();
 
-  // 0 available measurements?
+  // 0 measurements available?
   if (m == NULL) {
     // Can't determine rate -> can't determine seconds left.
     return -1;
   }
   
-  // 1 available measurement?
-  if (m == last_measurement()) {
-    // Can only determine seconds left if already 100%
-    return m->value >= target_value ? 0 : -1;
-  }
-
-  value_t work_left = target_value - last_measurement()->value;
-
-  if ((down ? -work_left : work_left) <= 0) {
+  // Target value reached?
+  if (down ? m->value <= target_value : m->value >= target_value) {
     return 0;
   }
   
+  // Only 1 measurement available?
+  if (m == last_measurement()) {
+    // Can't determine rate -> can't determine seconds left.
+    return -1;
+  }
+
+  // Note: work_left may be negative, but then so will average_rate be.
+  value_t work_left = target_value - last_measurement()->value;
   return work_left / average_rate();
 }
