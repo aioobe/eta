58b32aa2a4475ad9a12eacc2e4d7723b3e50a6c1
Andreas Lundblad <andreas.lundblad@gmail.com>
2019-02-17 07:11:49 +0100

    Changed ETA algorithm.

--- src/eta_calc.c
+++ src/eta_calc.c
@@ -1,6 +1,8 @@
 #include <stdbool.h>
 #include "eta_calc.h"
 
+#include <stdio.h>
+
 // Compute and return ratio completed
 double compute_ratio_completed(value_t initial_value, value_t target_value) {
   value_t value = last_measurement()->value;
@@ -16,12 +18,25 @@ double rate(Measurement *m1, Measurement *m2) {
   return progress_delta / time_delta;
 }
 
-// Calculate the weighted average rate of change, where the weights decrease
-// linearly the older they are.
+// Calculate the average rate of change.
 //
 // This function assumes that we have at least tmo measurements.
 double average_rate() {
+  
+  Measurement *oldest = oldest_measurement();
+  Measurement *latest = last_measurement();
 
+  return rate(oldest, latest);
+
+  // The problem with the calculation below is that when using --cont we may
+  // have a situation like this:
+  //
+  //   0 <500ms> 1 <500ms> 2 <1ms> 3 <500ms> 4 <500ms> ...
+  //
+  // The average rate of change for the 2 -> 3 delta will get huge and ruin the
+  // result completely.
+
+  /*
   // 1*rate[-k] + 2*rate[-k+1] + ... + k*rate[0]
   // -------------------------------------------
   //           1 + 2 + 3 + ... + k
@@ -37,7 +52,9 @@ double average_rate() {
     denominator += weight;
     weight++;
   } while (m != last_measurement());
+
   return numerator / denominator;
+  */
 }
 
 // Calculate and return estimated seconds left.
